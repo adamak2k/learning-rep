9573150a84532f419a7ca8bb7a52501ae649ab1e
adamak2k <adamak2k@gmail.com>
2017-07-10 14:02:29 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var y=0;
+
 document.getElementById("timeplace").innerHTML=Date();
 document.getElementById("losttime").innerHTML="You are "+y+" seconds on this page.";
-document.getElementById("inputtext").value="";
 
-setInterval(function Time(){
+setInterval(function GetDate(){
   document.getElementById("timeplace").innerHTML=Date();
 }, 1000);
 
-setInterval(function Time(){
+setInterval(function TimeSpend(){
   y=y+1;
   document.getElementById("losttime").innerHTML="You are "+y+" seconds on this page.";
 }, 1000);
